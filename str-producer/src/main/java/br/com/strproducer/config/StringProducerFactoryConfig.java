5deb65f6ebee67b27e99bae46db4df62f0e9aaa4
jefferson-git <jefferson-goiania@hotmail.com>
2023-01-18 09:10:01 -0300

    criando o Produtor de mensagens ProducerFactory

---
+++ str-producer/src/main/java/br/com/strproducer/config/StringProducerFactoryConfig.java
@@ -0,0 +1,35 @@
+package br.com.strproducer.config;
+
+import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+
+@Configuration
+@AllArgsConstructor
+public class StringProducerFactoryConfig {
+
+    private final KafkaProperties properties;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        var config = new HashMap<String, Object>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> producerTemplate(ProducerFactory<String, String> producerFactory){
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
